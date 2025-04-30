7550a80fcaa6f94ad2527ef3d650611b87373e8c
Dharav0126 <dharav2626@gmail.com>
2025-04-30 16:08:01 -0400

    testing nodemailer in production

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -4,14 +4,15 @@ exports.handleContact = async (req, res) => {
   const { name, email, number, subject, textarea } = req.body;
 
   if (!name || !email || !subject || !textarea) {
-    return res.status(400).json({ message: "Required fields are missing." });
+    return res.status(400).json({ message: "Required fields missing." });
   }
 
   try {
     await sendEmail({ name, email, number, subject, textarea });
-    res.status(200).json({ message: "Message sent successfully" });
+    return res.status(200).json({ message: "Message sent successfully" });
   } catch (error) {
-    console.error("Email send error:", error);
-    res.status(500).json({ message: "Failed to send message" });
+    console.error("Mail send error:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
