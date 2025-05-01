a7b0bd9466a52b1ce49e3f7d16a858e1c1327caa
Dharav0126 <dharav2626@gmail.com>
2025-05-01 19:59:07 -0400

    implimented direct key value for email

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -6,8 +6,8 @@ const sendEmail = async ({ name, email, number, subject, textarea }) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: "dharav2626@gmail.com",
+      pass: "kcic plph qvjs ssek",
     },
   });
 
