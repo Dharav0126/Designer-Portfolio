9140c547e34de2ff2ab77d59b625b539be1366b3
Dharav0126 <dharav2626@gmail.com>
2025-05-03 12:40:00 -0400

    optional port removed

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ app.get('/send-mail', (req,res)=>{
 
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on http://127.0.0.1:${PORT}`);
 });
